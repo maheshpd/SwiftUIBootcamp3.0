dbaa65f569dc2f07e6b211f587222f7b9bdcb9f7
maheshprasad <mahesh223prasad@gmail.com>
2022-04-09 03:30:37 +0530

    initial commit

--- SwiftUIBootcamp/ContentView.swift
+++ SwiftUIBootcamp/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello Mahesh, Welcome to the best SwiftUI Bootcamp ever!!")
+            .font(.system(size: 32, weight: .semibold, design: .serif))
+            .fontWeight(.heavy)
+            .foregroundColor(.pink)
+            .multilineTextAlignment(.center)
+            
+        
     }
 }
 
